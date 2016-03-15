ac203b08e8906c25fd3c45ebc2f6f7d5e8b9d111
Marijn Ophorst <marijn@sensimedia.nl>
2016-03-15 19:55:09 +0100

    reimplement and make simpler

--- src/Scheduler.php
+++ src/Scheduler.php
@@ -2,62 +2,31 @@
 
 namespace Croney;
 
-use PDO;
-use StdClass;
+use InvalidArgumentException;
+use Exception;
+use Monolog\Logger;
 
 class Scheduler
 {
-    private $pdo;
-    private $stmt;
-    private $allJobs;
-    private $getJobs;
-    private $reset;
-    private $start;
     private $now;
+    private $minutes = 1;
     private $jobs = [];
+    private $logger;
 
-    public function __construct(PDO $pdo)
+    public function __construct(Logger $logger = null)
     {
         $this->now = strtotime(date('Y-m-d H:i:00'));
-        $this->pdo = $pdo;
-        $this->stmt = new StdClass;
-        $this->stmt->check = $this->pdo->prepare(
-            "SELECT id FROM croney_job"
-        );
-        $this->stmt->add = $this->pdo->prepare(
-            "INSERT INTO croney_job (id, running, datedue)
-                VALUES (?, 0, ?)");
-        $this->stmt->due = $this->pdo->prepare(sprintf(
-            "SELECT * FROM croney_job WHERE
-                datedue <= %s
-                AND running = '0'",
-            $this->pdo->quote(date('Y-m-d H:i:00', $this->now))
-        ));
-        $this->stmt->reset = $this->pdo->prepare(
-            "UPDATE croney_job SET running = '0',
-                datedue = ? WHERE id = ?");
-        $this->stmt->start = $this->pdo->prepare(
-            "UPDATE croney_job SET running = '1' WHERE id = ?");
+        $this->logger = $logger;
     }
 
     /**
-     * Add a named job to the schedule.
+     * Add a job to the schedule.
      *
-     * @param string $id The unique name (id) of the job.
-     * @param string $when String describing when the task should run.
-     *  Can be either in a format `strtotime` understands, or a regular
-     *  expression the current date/time must match.
      * @param callable $job The job.
-     * @return self The scheduler, for chaining.
      */
-    public function task($id, $when, callable $job)
+    public function task(callable $job)
     {
-        if (!isset($when)) {
-            $when = '+1 minute';
-        }
-        $this->jobs[$id] = [$when, $job];
-
-        return $this;
+        $this->jobs[] = $job;
     }
 
     /**
@@ -65,56 +34,56 @@ class Scheduler
      */
     public function process()
     {
-        // First, clean up stale jobs and insert new ones.
-        $this->stmt->check->execute();
-        $all = [];
-        while (false !== ($id = $this->stmt->check->fetchColumn())) {
-            $all[] = $id;
-        }
-        $ids = array_keys($this->jobs);
-        array_walk($ids, function (&$id) use ($all) {
-            if (!in_array($id, $all)) {
-                $this->stmt->add->execute([
-                    $id,
-                    $this->toDateTime($this->jobs[$id][0]),
-                ]);
+        do {
+            $start = time();
+            array_walk($this->jobs, function ($job, $idx) {
+                try {
+                    $job->call($this);
+                } catch (NotDueException $e) {
+                } catch (Exception $e) {
+                    if ($this->logger) {
+                        $this->logger->addCritial($e->getMessage());
+                    }
+                }
+            });
+            if ($this->minutes > 1) {
+                sleep(60 - (time() - $start));
+                $this->now += 60;
             }
-            $id = $this->pdo->quote($id);
-        });
-        $this->pdo->exec(sprintf(
-            "DELETE FROM croney_job WHERE id NOT IN (%s)",
-            implode(', ', $ids)
-        ));
-
-        $this->stmt->due->execute();
-        $jobs = $this->stmt->due->fetchAll(PDO::FETCH_ASSOC);
-        $due = [];
-        array_walk($jobs, function ($job) use (&$due) {
-            $this->stmt->start->execute([$job['id']]);
-            $due[$job['id']] = $this->jobs[$job['id']];
-        });
-        array_walk($due, function ($job, $id) {
-            $this->stmt->start->execute([$id]);
-            call_user_func($job[1]);
-            $this->stmt->reset->execute([
-                $this->toDateTime($job[0]),
-                $id
-            ]);
-        });
+        } while (--$this->minutes);
     }
 
-    protected function toDateTime($when)
+    /**
+     * The job should run "at" the specified time.
+     *
+     * @param string $datestring A string parsable by `date` that should match
+     *  the current script runtime for the job to execute.
+     * @throws Croney\NotDueException if the task isn't due yet.
+     */
+    public function at($datestring)
     {
-        $when = preg_replace_callback(
-            "@[dDjlNSwzWFmMntLoYyaABgGhHisueIOPTZcrU]@",
-            function ($match) {
-                return '\\'.$match[0];
-            },
-            $when
-        );
-        $when = date($when);
-        $when = strtotime($when);
-        return date('Y-m-d H:i:00', $when);
+        $date = date('Y-m-d H:i:00', strtotime(date($datestring, $this->now)));
+        if (!preg_match("@^$date$@", date('Y-m-d H:i:00', $this->now))) {
+            throw new NotDueException;
+        }
+    }
+
+    /**
+     * Set the number of minutes this process should run.
+     *
+     * All jobs are run every minute, hence setting this to '5' would cause the
+     * loop to run 5 times. After each loop, the scheduler `wait`s for sixty
+     * seconds (minus the seconds it took the loop to run) before starting the
+     * next run.
+     *
+     * @param int $minutes
+     */
+    public function setDuration($minutes)
+    {
+        if (!is_integer($minutes)) {
+            throw new InvalidArgumentException('$minutes must be an integer.');
+        }
+        $this->minutes = $minutes;
     }
 }
 
