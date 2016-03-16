d8bd79a7d578b8423092e9d3f2de052bc6d6bf50
Marijn Ophorst <marijn@sensimedia.nl>
2016-03-16 18:39:01 +0100

    improve docblock

--- src/Scheduler.php
+++ src/Scheduler.php
@@ -85,9 +85,13 @@ class Scheduler extends ArrayObject
      * Set the number of minutes this process should run.
      *
      * All jobs are run every minute, hence setting this to '5' would cause the
-     * loop to run 5 times. After each loop, the scheduler `wait`s for sixty
+     * loop to run 5 times. After each loop, the scheduler `sleep`s for sixty
      * seconds (minus the seconds it took the loop to run) before starting the
      * next run.
+     *
+     * Note that this does not guarantee the scheduler will resume _exactly_ on
+     * the next minute. If your task involves handling based on e.g. `time()`,
+     * make sure to round/truncate/check its value.
      *
      * @param int $minutes
      */
